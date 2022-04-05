a816b667fdcd141f769a6a3d1de53ab1769e526f
Lee <lj910039885@gmail.com>
2022-04-05 14:59:17 +0800

    vue

---
+++ vue_test/04_src_mixin混入(合)/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Student name="李四" sex="女" :age="18"/>
+    <hr>
+    <Student name="王四" sex="男" :age="22"/>
+  </div>
+</template>
+
+<script>
+// 引入School组件
+import Student from './components/Student.vue'
+export default {
+  name: 'App',
+  // eslint-disable-next-line vue/no-unused-components
+  components:{Student},
+}
+</script>
+
+<style>
+
+</style>
--- vue_test/04_src_mixin混入(合)/components/School.vue
+++ vue_test/04_src_mixin混入(合)/components/School.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h2 @click="showName">学生姓名：{{name}}</h2>
-		<h2>学生性别：{{sex}}</h2>
+    <h2 @click="showName">学校名称：{{ name }}</h2>
+    <h2>学校地址：{{ address }}</h2>
   </div>
 </template>
 
@@ -12,13 +12,14 @@ import {hunhe, hunhe2} from '../mixin.js'
 
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name:'Student',
-		data() {
-			return {
-				name:'张三',
-				sex:'男'
-			}
-		},
+  name: 'School',
+  data() {
+    return {
+      name: '尚硅谷',
+      address: '北京',
+      x: 666
+    }
+  },
   mixins: [hunhe, hunhe2]
 }
 </script>
