f4820b4a0b81e94faf9e3830ac1ee6f72621569d
Lee <lj910039885@gmail.com>
2022-04-09 17:52:17 +0800

    vue

---
+++ vue_test/src/components/Test3.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<button @click="isShow = !isShow">显示/隐藏</button>
+		<transition-group 
+			appear
+			name="animate__animated animate__bounce" 
+			enter-active-class="animate__swing"
+			leave-active-class="animate__backOutUp"
+		>
+			<h1 v-show="!isShow" key="1">你好啊！</h1>
+			<h1 v-show="isShow" key="2">尚硅谷！</h1>
+		</transition-group>
+	</div>
+</template>
+
+<script>
+	import 'animate.css'
+	export default {
+		// eslint-disable-next-line vue/multi-word-component-names
+		name:'Test',
+		data() {
+			return {
+				isShow:true
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	h1{
+		background-color: orange;
+	}
+
+	.hello-enter-active{
+		animation: atguigu 0.5s linear;
+	}
+
+	.hello-leave-active{
+		animation: atguigu 0.5s linear reverse;
+	}
+
+	@keyframes atguigu {
+		from{
+			transform: translateX(-100%);
+		}
+		to{
+			transform: translateX(0px);
+		}
+	}
+</style>
