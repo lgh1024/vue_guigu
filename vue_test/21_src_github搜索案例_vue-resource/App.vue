0201e95262d709f174ebcb339ee0af2fe0d0a661
Lee <lj910039885@gmail.com>
2022-04-10 11:33:48 +0800

    vue

---
+++ vue_test/21_src_github搜索案例_vue-resource/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="container">
+		<Search></Search>
+		<List></List>
+	</div>
+</template>
+
+<script>
+import Search from './compontents/Search.vue'
+import List from './compontents/List.vue'
+	export default {
+		name:'App',
+		components: {Search, List}
+	}
+</script>
