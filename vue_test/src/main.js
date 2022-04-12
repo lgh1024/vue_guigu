1a5e3d325ef550df4c2b17e97414b1654a85ff23
Lee <lj910039885@gmail.com>
2022-04-12 23:57:28 +0800

    vue

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -2,21 +2,19 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-// 引入插件
-import vueResource from 'vue-resource'
-// 引入store
-import store from './store'
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-//使用插件
-Vue.use(vueResource)
+//应用插件
+Vue.use(VueRouter)
 
 //创建vm
 new Vue({
 	el:'#app',
 	render: h => h(App),
-	store,
-	beforeCreate() {
-		Vue.prototype.$bus = this
-	}
+	router:router
 })
