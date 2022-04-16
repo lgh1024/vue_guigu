e3964dcd90bca9c3e87fb6d9b389bff2d3b8aeee
Lee <lj910039885@gmail.com>
2022-04-16 15:32:53 +0800

    vue

---
+++ vue3_test/19_src_provide与inject/components/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="child">
+    <h3>我是Child组件（子）</h3>
+    <Son></Son>
+  </div>
+</template>
+
+<script>
+  // import {inject} from 'vue'
+  import Son from './Son.vue'
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Child',
+    components: {Son}
+    /* setup(){
+			let x = inject('car')
+			console.log(x,'Child-----')
+		} */
+  }
+</script>
+
+<style>
+	.child{
+		background-color: skyblue;
+		padding: 10px;
+	}
+</style>
